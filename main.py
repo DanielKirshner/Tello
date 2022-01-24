09a40bd0e11a50c98eab7fb3ef223daecfc79485
DanielKirshner <danielkirshner7878@gmail.com>
2022-01-24 21:34:51 +0200

    Added main function

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from djitellopy import tello
 
-TELLO_IP = '192.168.10.1'
-TELLO_PORT = '8889'
 
-print("Connecting to TELLO...")
-me = tello.Tello()
-me.connect()
+def main():
+    me = tello.Tello()
+    me.connect()
+    print(f"{me.get_battery()}%")
 
-print(f"{me.get_battery()}%")
+
+if __name__ == '__main__':
+    main()
