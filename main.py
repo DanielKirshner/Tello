8a8f32e011c04b96487772888eca1c2181f6086c
DanielKirshner <danielkirshner7878@gmail.com>
2022-01-25 00:50:45 +0200

    Added stream_video_from_drone function with cv2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from djitellopy import tello
 from rich import print
+import cv2
+
+TELLO_IP = '192.168.10.1'
+TELLO_PORT = '8889'
 
 
 def print_battery_drone(my_tello):
@@ -12,10 +16,22 @@ def print_battery_drone(my_tello):
         print(f"[bold red]Battery = {bat}%")
 
 
+def stream_video_from_drone(my_tello):
+    my_tello.streamon()
+
+    while True:
+        img = my_tello.get_frame_read().frame
+        img = cv2.resize(img, (360, 240)) # There is a small delay so smaller resolution is better
+        cv2.imshow("Tello Live", img)
+        cv2.waitKey(1)
+
+
 def main():
     me = tello.Tello()
     me.connect()
     print_battery_drone(me)
+
+    stream_video_from_drone(me)
 
 
 if __name__ == '__main__':
