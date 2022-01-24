2de7da60386cdc9dbb45a7d0c52fbbcaeb8b587a
DanielKirshner <danielkirshner7878@gmail.com>
2022-01-25 00:41:43 +0200

    Print the drone battery precentage colored - according to the value

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from djitellopy import tello
+from rich import print
+
+
+def print_battery_drone(my_tello):
+    bat = my_tello.get_battery()
+    if bat >= 65:
+        print(f"[bold green]Battery = {bat}%")
+    elif bat >= 25:
+        print(f"[bold yellow]Battery = {bat}%")
+    else:
+        print(f"[bold red]Battery = {bat}%")
 
 
 def main():
     me = tello.Tello()
     me.connect()
-    print(f"{me.get_battery()}%")
+    print_battery_drone(me)
 
 
 if __name__ == '__main__':
